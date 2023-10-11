2c6c5d8a2e332a765a8b34aabceff501849b5bd5
cat <cat@shrineforest.nl>
2023-10-11 16:03:00 +0200

    removed plantuml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
-import simplePlantUML from "@akebifiky/remark-simple-plantuml";
-
 export default {
   markdown: {
-    // Applied to .md and .mdx files
-    remarkPlugins: [
-      [simplePlantUML, { baseUrl: "https://git.ironsm4sh.nl/-/plantuml/svg" }],
-    ],
   },
 };
